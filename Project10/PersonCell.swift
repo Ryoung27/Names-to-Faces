081928819f8b898790818fdeba8a65f3a7bcb76d
Ryoung27 <RRYoung89@gmail.com>
2018-09-23 08:44:37 -0500

    User cells built and connected.

---
+++ Project10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Richard Young on 9/23/18.
+//  Copyright © 2018 Richard Young. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
